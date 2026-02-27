bb3b647e1e9ca7b41e3d078a4ffc08122592253c
K.A Lacsita <kalac@example.com>
2026-02-27 09:15:13 +0530

    UC13 Performance measurement added

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,8 @@
-// UC12 — Strategy Pattern
-
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean check(String text);
-}
+public class PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String text) {
+    static boolean checkPalindrome(String text) {
         Stack<Character> stack = new Stack<>();
 
         for (char c : text.toCharArray()) {
@@ -21,36 +16,17 @@ class StackStrategy implements PalindromeStrategy {
 
         return text.equals(reversed);
     }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-    public boolean check(String text) {
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : text.toCharArray()) {
-            deque.add(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "level";
 
-        // Strategy selection (runtime change possible)
-        PalindromeStrategy strategy = new StackStrategy();
-        // PalindromeStrategy strategy = new DequeStrategy();
+        long start = System.nanoTime();
+        boolean result = checkPalindrome(input);
+        long end = System.nanoTime();
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome : " + strategy.check(input));
+        System.out.println("Is Palindrome : " + result);
+        System.out.println("Execution Time : " + (end - start) + " ns");
     }
 }
